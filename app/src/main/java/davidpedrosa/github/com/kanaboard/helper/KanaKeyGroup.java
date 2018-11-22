a1cfc005d7c2e34f4ed5f51fe0dd681c0876ecc5
David Pedrosa <d.pedrosa@indizen.com>
2018-11-22 08:03:01 +0100

    BUG-FIX.

--- app/src/main/java/davidpedrosa/github/com/kanaboard/helper/KanaKeyGroup.java
+++ app/src/main/java/davidpedrosa/github/com/kanaboard/helper/KanaKeyGroup.java
@@ -6,7 +6,8 @@ public abstract class KanaKeyGroup
     public abstract char [][] getNormalKeys();
     public abstract char [][] getVoicedKeys();
     public abstract char [][] getSemiVoicedKeys();
-    
+
+    private static final int [] voicedRowIndexes = {1,2,3,5};
     public char [][] getKeys(KanaKeyStatus status) {
         char [][] keys = getNormalKeys();
         char [][] voicedKeys = getVoicedKeys();
@@ -17,8 +18,10 @@ public abstract class KanaKeyGroup
             System.arraycopy(keys[i], 0, result[i], 0,  keys[i].length);
         }
         if (status.equals(KanaKeyStatus.VOICED)) {
-            for (int i = 1; i< 4; i++) {
-                System.arraycopy(voicedKeys[i - 1], 0, result[i], 0,  voicedKeys[0].length);
+            int k = 0;
+            for (int i: voicedRowIndexes) {
+                System.arraycopy(voicedKeys[k], 0, result[i], 0,  voicedKeys[k].length);
+                k++;
             }
         } else if (status.equals(KanaKeyStatus.SEMI_VOICED)) {
             System.arraycopy(semiVoicedKeys[0], 0, result[5], 0,  semiVoicedKeys[0].length);
